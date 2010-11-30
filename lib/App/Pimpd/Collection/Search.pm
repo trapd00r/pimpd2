6fa98f0f0bac431698696c0ca0539bdf61bca122
Magnus Woldrich <trapd00r@trapd00r.se>
2010-11-30 10:19:21 +0100

    Collection::Search: cleanup

--- lib/App/Pimpd/Collection/Search.pm
+++ lib/App/Pimpd/Collection/Search.pm
@@ -4,9 +4,11 @@ package App::Pimpd::Collection::Search;
 require Exporter;
 @ISA = 'Exporter';
 
-our @EXPORT = qw(search_db);
+our @EXPORT = qw(search_db_quick);
 
+use strict;
 use App::Pimpd qw($mpd);
+use Carp 'confess';
 use Data::Dumper;
 $Data::Dumper::Terse     = 1;
 $Data::Dumper::Indent    = 1;
@@ -17,6 +19,35 @@ $Data::Dumper::Sortkeys  = 1;
 
 
 sub search_db {
+  my $query = shift;
+
+  if(!defined($query)) {
+    return undef;
+  }
+
+  if(invalid_regex($query)) {
+    confess("Invalid regex '$query'");
+  }
+
+  my @result;
+
+  my %playlist;
+  for($mpd->playlist->as_items) {
+    $playlist{pos}{$_->pos} = join(' - ', $_->artist, $_->album, $_->title);
+  }
+  #print Dumper \%playlist;
+
+  for my $pos(keys(%{$playlist{pos}})) {
+    if($playlist{pos}->{$pos} =~ /$query/pig) {
+      push(@result, $pos);
+    }
+  }
+  return (wantarray()) ? @result : scalar(@result);
+}
+
+
+1;
+sub search_db_quick {
   my $query = shift;
 
   my @result;
