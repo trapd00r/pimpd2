4aa052ab625155cb492b443b75da436155d7ef91
Magnus Woldrich <trapd00r@trapd00r.se>
2010-11-30 11:12:09 +0100

    Pimpd.pm: require a temp config to access the user supplied colors

--- lib/App/Pimpd.pm
+++ lib/App/Pimpd.pm
@@ -6,7 +6,7 @@ $VERSION = 0.01;
 
 require Exporter;
 @ISA = 'Exporter';
-our @EXPORT = qw($mpd invalid_regex);
+our @EXPORT = qw($mpd invalid_regex @c);
 
 use lib '/home/scp1/devel/pimpd-ng2/lib';
 
@@ -18,3 +18,8 @@ our $mpd = Audio::MPD->new(
   host  => $ENV{MPD_HOST},
   port  => $ENV{MPD_PORT},
 );
+
+our @c;
+
+my $config = './pimpd.conf';
+require($config);
