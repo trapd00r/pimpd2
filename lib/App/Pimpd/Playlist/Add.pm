34389752ec2512050553d59a3a124a93bcbd237c
Magnus Woldrich <trapd00r@trapd00r.se>
2010-11-30 10:19:43 +0100

    Playlist::Add: accept arrayrefs, cleanup

--- lib/App/Pimpd/Playlist/Add.pm
+++ lib/App/Pimpd/Playlist/Add.pm
@@ -6,6 +6,7 @@ require Exporter;
 
 our @EXPORT = qw(add_to_playlist);
 
+use strict;
 use Data::Dumper;
 $Data::Dumper::Terse     = 1;
 $Data::Dumper::Indent    = 1;
@@ -17,18 +18,30 @@ $Data::Dumper::Sortkeys  = 1;
 use App::Pimpd;
 
 
+=head3 add_to_playlist()
+
+  add_to_playlist($file);
+  add_to_playlist(\@files);
+
+Add file to the current playlist.
+
+File must be in the MPD format, without the music_directory prefix.
+
+=cut
+
 sub add_to_playlist {
   my $song = shift;
 
   my @songs;
   if(ref($song) eq 'ARRAY') {
     push(@songs, @{$song});
-    undef($song);
   }
-  else {
-    return "This is a scalar: $song\n";
-  }
-  return "Arrayref I got: @songs";
+   else {
+     $mpd->playlist->add($song);
+     return 0;
+   }
+   $mpd->playlist->add(@songs);
+   return 0;
 }
 
 
