cfbf7366cc8d3dbe4e7db0b274c639d0fc941e54
Magnus Woldrich <trapd00r@trapd00r.se>
2010-12-02 19:49:51 +0100

    Playlist::Randomize: new functions randomize_albums() and randomize_track_in_playlist()

--- lib/App/Pimpd/Playlist/Randomize.pm
+++ lib/App/Pimpd/Playlist/Randomize.pm
@@ -4,7 +4,13 @@ package App::Pimpd::Playlist::Randomize;
 require Exporter;
 @ISA = 'Exporter';
 
-our @EXPORT = qw(randomize);
+our @EXPORT = qw(
+  randomize
+  randomize_albums
+  random_track_in_playlist
+);
+
+
 
 use strict;
 use Carp;
@@ -23,8 +29,9 @@ use List::Util 'shuffle';
 
 =head3 randomize()
 
-Return n random tracks in an arrayref.
+Parameters: $integer
 
+Returns:    \@files
 
 =cut
 
@@ -35,6 +42,47 @@ sub randomize {
   my @random = (@songs[0 .. $no_songs - 1]);
 
   return \@random;
+}
+
+=head3 randomize_albums()
+
+Parameters: $integer
+
+Returns:    @files
+
+=cut
+
+sub randomize_albums {
+  my $no_albums = shift // 10;
+  my @albums = shuffle($mpd->collection->all_albums);
+
+  my @songs;
+  for(@albums) {
+    if($no_albums == 0) {
+      last;
+    }
+    my @songs_on_album = $mpd->collection->songs_from_album($_);
+    if(scalar(@songs_on_album) <= 2) {
+      next;
+    }
+    else {
+      push(@songs, map { $_->file } @songs_on_album);
+      $no_albums--;
+    }
+  }
+  return (wantarray()) ? @songs : scalar(@songs);
+}
+
+sub random_track_in_playlist {
+  if(empty_playlist()) {
+    print STDERR "Playlist is empty - nothing to play\n";
+    return 1;
+  }
+  my @items = $mpd->playlist->as_items;
+
+  @items = shuffle(@items);
+
+  return $items[0]->pos;
 }
 
 
