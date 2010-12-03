3f1b096438234eeb6424dad62f7620db4d41d2c1
Magnus Woldrich <trapd00r@trapd00r.se>
2010-12-03 13:52:09 +0100

    Pimpd.pm: added pod

--- lib/App/Pimpd.pm
+++ lib/App/Pimpd.pm
@@ -6,7 +6,7 @@ $VERSION = 0.01;
 
 require Exporter;
 @ISA = 'Exporter';
-our @EXPORT = qw($mpd invalid_regex @c);
+our @EXPORT = qw($mpd @c);
 
 use lib '/home/scp1/devel/pimpd-ng2/lib';
 
@@ -22,3 +22,50 @@ our @c;
 
 my $config = './pimpd.conf';
 require($config);
+
+#!/usr/bin/perl
+our $APP     = undef;
+our $VERSION = '0.1.0';
+use strict;
+use Data::Dumper;
+use Pod::Usage;
+
+
+
+
+
+
+=pod
+
+=head1 NAME
+
+pimpd2 - Perl Interface for the Music Player Daemon 2
+
+=head1 SYNOPSIS
+
+=head3 Usage
+
+  pimpd2 [OPTION] [FILE...]
+
+=head1 DESCRIPTION
+
+pimpd rocks
+
+=head1 OPTIONS
+
+=head1 AUTHOR
+
+Written by Magnus Woldrich
+
+=head1 REPORTING BUGS
+
+Report bugs to trapd00r@trapd00r.se
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Magnus Woldrich
+
+License GPLv2
+
+=cut
+
