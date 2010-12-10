1d5526980eed7d0ecbb6c3f53ce059b4fdc2d052
Magnus Woldrich <trapd00r@trapd00r.se>
2010-12-10 09:27:28 +0100

    Collection::Search: added full POD

--- lib/App/Pimpd/Collection/Search.pm
+++ lib/App/Pimpd/Collection/Search.pm
@@ -12,26 +12,10 @@ our @EXPORT = qw(
   );
 
 use strict;
-use App::Pimpd qw($mpd);
+use App::Pimpd;
 use App::Pimpd::Validate;
 use Carp 'confess';
 
-
-=pod
-
-=head1 NAME
-
-App::Pimpd::Collection::Search - search the MPD collection
-
-=head2 search_db_quick()
-
-  my @files = search_db_quick($query);
-  my $finds = search_db_quick($query);
-
-In list context, returns all files matching pattern.
-In scalar context, returns the number of matches.
-
-=cut
 
 sub search_db_quick {
   my $query = shift;
@@ -49,17 +33,6 @@ sub search_db_quick {
   return (wantarray()) ? @result : scalar(@result);
 }
 
-=head2 search_db_artist()
-
-  my @artists = search_db_artist($artist);
-
-In list context, returns all files where the artist fields partially matches
-str.
-
-In scalar context, returns the number of finds.
-
-=cut
-
 sub search_db_artist {
   my $artist  = shift; # Not a regex
 
@@ -74,17 +47,6 @@ sub search_db_artist {
   return (wantarray()) ? @tracks : scalar(@tracks);
 }
 
-=head2 search_db_title()
-
-  my @titles = search_db_title($title);
-
-In list context, returns all files where the title fields partially matches
-str.
-
-In scalar context, returns the number of finds.
-
-=cut
-
 sub search_db_title {
   my $title  = shift;
   my @titles = $mpd->collection->songs_with_title_partial($title);
@@ -98,17 +60,6 @@ sub search_db_title {
   return (wantarray()) ? @titles : scalar(@titles);
 }
 
-=head2 search_db_album()
-
-  my @albums = search_db_album($album);
-
-In list context, returns all files where the album fields partially matches
-str.
-
-In scalar context, returns the number of finds.
-
-=cut
-
 sub search_db_album {
   my $album  = shift;
   my @albums = $mpd->collection->songs_from_album_partial($album);
@@ -121,7 +72,91 @@ sub search_db_album {
   return (wantarray()) ? @albums : scalar(@albums);
 }
 
+=pod
 
+=head1 NAME
 
+App::Pimpd::Collection::Search - Package exporting various search functions for
+the MPD collection
+
+=head1 SYNOPSIS
+
+    use App::Pimpd;
+    use App::Pimpd::Collection::Search
+
+    my @album  = search_db_album('Stripped');
+    my @songs  = search_db_quick('love');
+
+=head1 DESCRIPTION
+
+App::Pimpd::Collection::Search provides search functions for the MPD collection
+
+=head1 EXPORTS
+
+=head2 search_db_quick()
+
+  my @paths = search_db_quick('foo');
+
+Parameters: $regex
+
+Returns:    @paths
+
+Given a valid regular expression, searches the collection for matching
+filenames. The search is performed case insensitive.
+
+In list context, returns full paths for the matched songs.
+
+In scalar context, returns the number of matches.
+
+=head2 search_db_artist()
+
+  my @paths = search_db_artist('Laleh');
+
+Parameters: $string
+
+Returns:    @paths
+
+In list context, returns full paths for all songs by $artist.
+
+In scalar context, returns the number of songs by $artist.
+
+=head2 search_db_album()
+
+Parameters: $string
+
+Returns:    @paths
+
+In list context, returns full paths for the songs on $album.
+
+In scalar context, returns the number of songs on albums.
+
+=head2 search_db_title()
+
+Parameters: $string
+
+Returns:    @paths
+
+In list context, returns full paths for the songs named $string.
+
+In scalar context, returns the number of songs named $string.
+
+=head1 SEE ALSO
+
+App::Pimpd::Collection
+
+=head1 AUTHOR
+
+  Magnus Woldrich
+  CPAN ID: WOLDRICH
+  magnus@trapd00r.se
+  http://japh.se
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Magnus Woldrich. All right reserved.
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
 
 1;
