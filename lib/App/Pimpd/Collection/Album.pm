99af6a3adb9a2c13fa8a416579ebfcfb3d47967c
Magnus Woldrich <trapd00r@trapd00r.se>
2010-12-10 09:08:47 +0100

    Collection::Album: added full POD

--- lib/App/Pimpd/Collection/Album.pm
+++ lib/App/Pimpd/Collection/Album.pm
@@ -79,6 +79,75 @@ sub songs_on_album {
   return (wantarray()) ? @tracks : scalar(@tracks);
 }
 
+=pod
 
+=head1 NAME
+
+App::Pimpd::Collection::Album - Album functions
+
+=head1 SYNOPSIS
+
+    use App::Pimpd;
+    use App::Pimpd::Collection::Album;
+
+    my @albums = albums_by_artist('Laleh');
+    my @songs  = songs_on_album('Me and Simon');
+
+=head1 DESCRIPTION
+
+App::Pimpd::Collection::Album exports functions that provides album-specific data.
+
+=head1 EXPORTS
+
+=head2 albums_by_artist()
+
+  my @albums = albums_by_artist('Laleh');
+
+Parameters: $artist | NONE
+
+Returns:    @albums | scalar(@albums)
+
+In list context, returns a sorted list with the albums (strings) where $artist is
+featured.
+
+In scalar context, returns the number of albums where $artist is featured.
+
+If called without arguments, the current artist is used.
+
+=head2 songs_on_album()
+
+  my @songs =  songs_on_album('Stripped'); # Christina Aguilera
+
+Parameters: $album, $artist | NONE
+
+Returns:    @paths          | scalar(@paths)
+
+In list context, returns a list with full paths to the songs on album.
+
+In scalar context, returns the number of files on the album.
+
+If called without arguments, the current album is used.
+
+Note that if called without a second argument, it can return both Bob Dylan
+and Britney Spears if the album is 'Best of'.
+
+=head1 SEE ALSO
+
+App::Pimpd::Collection
+
+=head1 AUTHOR
+
+  Magnus Woldrich
+  CPAN ID: WOLDRICH
+  magnus@trapd00r.se
+  http://japh.se
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Magnus Woldrich. All right reserved.
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
 
 1;
