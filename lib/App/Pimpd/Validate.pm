f26fba2055260e508df80e0ee66a7dbe4f5f18a4
Magnus Woldrich <m@japh.se>
2011-11-24 17:44:12 +0100

    deuglify escape regex a tiny bit

--- lib/App/Pimpd/Validate.pm
+++ lib/App/Pimpd/Validate.pm
@@ -93,7 +93,7 @@ sub isa_valid_playlist {
 
 sub escape {
   my $str = shift;
-  $str =~ s/([;<>\*\|`&\$!#\(\)\[\]\{\}:'"\n])/\\$1/gm;
+  $str =~ s/([;<>*|`&\$!#()[\]{}:'"\s])/\\$1/gm;
 
   return $str;
 }
